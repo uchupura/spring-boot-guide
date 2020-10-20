7fac04adc4deea16b7b754b86aeefbc8dc694606
jihyeon.jang <jihyeon.jang@sk.com>
2020-10-20 20:53:33 +0900

    [Redis] 클러스터링 Redis 가이드 추가

---
+++ application/spring-boot-redis/src/main/java/com/guide/redis/controller/RedisController.java
@@ -0,0 +1,37 @@
+package com.guide.redis.controller;
+
+import com.guide.redis.entity.Token;
+import com.guide.redis.repository.TokenReposiroty;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.HashOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class RedisController {
+
+    private final TokenReposiroty reposiroty;
+
+    @PostMapping
+    public void create(@RequestBody Token token) {
+
+        reposiroty.save(token);
+    }
+
+    @GetMapping("{id}")
+    public Token read(@PathVariable("id") String id) {
+        return reposiroty.findById(id).orElse(null);
+    }
+
+        @PutMapping
+    public Token update(@RequestBody Token token) {
+        return reposiroty.save(token);
+    }
+
+    @DeleteMapping("{id}")
+    public void delete(@PathVariable("id") String id) {
+        reposiroty.deleteById(id);
+    }
+}
